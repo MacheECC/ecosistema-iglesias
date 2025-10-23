16b4de37dd550dc44aa02c6e47eeae94cade85ac
Marcelo Contreras <mache@Marcelos-MacBook-Air.local>
2025-10-23 16:45:40 -0400

    Add filesystem hook: rls-context

--- extensions/hooks/rls-context/index.js
+++ extensions/hooks/rls-context/index.js
@@ -1,4 +1,7 @@
+// index.js
 export default ({ init, logger }) => {
   logger.info('RLS HOOK: filesystem hook file was loaded');
-  init('server.start', () => logger.info('RLS HOOK: server.start observed'));
+  init('server.start', () => {
+    logger.info('RLS HOOK: server.start observed');
+  });
 };
